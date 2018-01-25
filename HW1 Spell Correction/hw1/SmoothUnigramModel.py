6889423a47aa0ed013946e08f16f3d285282b551
Kangbo Lu <redstonegranola@gmail.com>
2018-01-24 23:31:15 -0800

    upload smoothened unigram model

--- HW1 Spell Correction/hw1/SmoothUnigramModel.py
+++ HW1 Spell Correction/hw1/SmoothUnigramModel.py
@@ -4,6 +4,8 @@ class SmoothUnigramModel:
 
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
+    self.totalCount = 0
+    self.unigramCounts = collections.defaultdict(lambda: 0)
     self.train(corpus)
 
   def train(self, corpus):
@@ -12,14 +14,27 @@ class SmoothUnigramModel:
     """  
     # TODO your code here
     # Tip: To get words from the corpus, try
-    #    for sentence in corpus.corpus:
-    #       for datum in sentence.data:  
-    #         word = datum.word
-    pass
+    for sentence in corpus.corpus:
+      for datum in sentence.data:  
+        self.unigramCounts[datum.word] += 1
+        self.totalCount += 1
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
     # TODO your code here
-    return 0.0
+    score = 0.0
+    zeroCount = 0
+
+    # count each incremented word
+    for word in sentence:
+      if word not in self.unigramCounts:
+        zeroCount += 1
+
+    # apply laplace smoothing to unigram model
+    for word in sentence:
+      count = self.unigramCounts[word]
+      score += math.log(count + 1)
+      score -= math.log(self.totalCount + zeroCount)
+    return score
