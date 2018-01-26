f073235b3bffc01e77ceb78a92f514d0daabb313
Kangbo Lu <redstonegranola@gmail.com>
2018-01-26 10:39:10 -0800

    adding smoothened unigram and bigram model

--- HW1 Spell Correction/hw1/SmoothBigramModel.py
+++ HW1 Spell Correction/hw1/SmoothBigramModel.py
@@ -4,22 +4,45 @@ class SmoothBigramModel:
 
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
+    self.bigramCounts = collections.defaultdict(lambda: 0) # dictionary for bigram
+    self.previousCounts = collections.defaultdict(lambda: 0) # dictionary for unigram of current word
+    self.zeroCount = 0
     self.train(corpus)
 
   def train(self, corpus):
     """ Takes a corpus and trains your language model. 
         Compute any counts or other corpus statistics in this function.
-    """  
+    """
     # TODO your code here
     # Tip: To get words from the corpus, try
-    #    for sentence in corpus.corpus:
-    #       for datum in sentence.data:  
-    #         word = datum.word
-    pass
+    for sentence in corpus.corpus:
+      for datumIndex in xrange(1, len(sentence.data)):
+        datum = sentence.data
+        bigram = datum[datumIndex].word + datum[datumIndex-1].word # get bigram string
+        previousWord = datum[datumIndex-1].word # get previous word
+        self.bigramCounts[bigram] += 1 # count number of bigram
+        self.previousCounts[previousWord] += 1 # count number of previous word
 
+  # P(current | previous) for bigram model
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
     # TODO your code here
-    return 0.0
+    score = 0.0 # initialize score
+
+    # calculate not seen bigram for V for laplace smoothing
+    for wordIndex in xrange(1, len(sentence)):
+      bigram = sentence[wordIndex] + sentence[wordIndex-1]
+      if bigram not in self.bigramCounts:
+        self.zeroCount += 1
+
+    # apply laplace smoothing to the bigram model
+    for wordIndex in xrange(1, len(sentence)):
+      bigramCount = self.bigramCounts[sentence[wordIndex] + sentence[wordIndex-1]] # count bigram
+      previousCount = self.previousCounts[sentence[wordIndex-1]] # count previous word
+
+      # calculate P(current | previous)
+      score += math.log(bigramCount + 1)
+      score -= math.log(previousCount + self.zeroCount)
+    return score
--- HW1 Spell Correction/hw1/SmoothUnigramModel.py
+++ HW1 Spell Correction/hw1/SmoothUnigramModel.py
@@ -6,6 +6,7 @@ class SmoothUnigramModel:
     """Initialize your data structures in the constructor."""
     self.totalCount = 0
     self.unigramCounts = collections.defaultdict(lambda: 0)
+    self.zeroCount = 0
     self.train(corpus)
 
   def train(self, corpus):
@@ -18,23 +19,22 @@ class SmoothUnigramModel:
       for datum in sentence.data:  
         self.unigramCounts[datum.word] += 1
         self.totalCount += 1
-
+  # P(current) for unigram model
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
     # TODO your code here
     score = 0.0
-    zeroCount = 0
 
     # count each incremented word
     for word in sentence:
       if word not in self.unigramCounts:
-        zeroCount += 1
+        self.zeroCount += 1
 
     # apply laplace smoothing to unigram model
     for word in sentence:
       count = self.unigramCounts[word]
       score += math.log(count + 1)
-      score -= math.log(self.totalCount + zeroCount)
+      score -= math.log(self.totalCount + self.zeroCount)
     return score
